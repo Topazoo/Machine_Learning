88000bc383e151da93bebbcaa3592f9ef9c7b6e1
Peter Swanson <topazoo42@gmail.com>
2017-08-20 18:58:44 -0400

    Finished Euclidean Classifier

---
+++ collab_filter/euclid_dist/euclid_dist.py
@@ -0,0 +1,72 @@
+from math import sqrt
+import recommendations
+
+'''Code for Calculating Euclidean Distance Scores.
+   2 preferences are mapped on axes to determine
+   similarities''' 
+
+def distance(y1, y2, x1, x2):
+    '''Calculating Distance:
+        1. Take the difference in each axis
+        2. Square them
+        3. Add them
+        4. Take the sqaure root of the sum
+       
+       Returns a score between 0 (opposite) and 1
+       (identical)'''
+
+    dist = sqrt(pow(y1 - y2, 2)+pow(x1 - x2, 2))
+    
+    '''Invert distance so higher values mean higher 
+       similarities'''
+
+    inv_dist = 1/(1 + dist)
+
+    return inv_dist
+
+def calc_sim(pref_dict, cat1, cat2):
+    '''Calculates the similarity between two categories
+       and returns a Euclidean Distance score between 0
+       (opposite) and 1 (identical)
+
+        @pref_dict:  A dictionary of categories containing 
+                    dictionaries of items and scores.
+        @cat1: The first category to compare items from.
+        @cat2: The category to compare items 2.'''
+
+    shared = {} #Hold the items shared by both categories
+    dist_scores = []
+
+    '''Store all items that are in both categories'''
+
+    for item in pref_dict[cat1]:
+        if item in pref_dict[cat2]:
+            shared[item] = 1
+
+    '''If there are no shared items return 0'''
+
+    if len(shared) == 0:
+        return 0 
+
+    '''Calculate the Euclidean Distance of all shared
+       items and sum them using a list comprehension'''
+
+    for item in shared:
+        score = pow(pref_dict[cat1][item]-pref_dict[cat2][item],2)
+        dist_scores.append(score)
+
+    score_sum = sum(dist_scores)
+
+#    dist_scores = sum([pow(pref_dict[cat1][item]-pref_dict[cat2][item],2)
+ #                     for item in shared])
+
+    '''Invert the sum'''
+
+    invert_dist = 1/(1+sqrt(score_sum))
+    
+    return invert_dist
+
+print calc_sim(recommendations.critics, 'Lisa Rose', 'Gene Seymour')
+print calc_sim(recommendations.critics, 'Lisa Rose', 'Claudia Puig')
+
+
